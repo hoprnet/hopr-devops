d92a215e9444641bfbf81a633dadefb74573f070
Jose Aguinaga <me@jjperezaguinaga.com>
2020-05-15 14:47:34 +0200

    ♻️  Cleaned up code to fit fmt

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 provider "google" {
- project     = "hoprassociation"
- region      = "europe-west6"
+  project = "hoprassociation"
+  region  = "europe-west6"
 }
 
 // A single Google Cloud Engine instance
 resource "google_compute_instance" "default" {
- name         = "hopr-develop-eu-core-001-west6-a"
- machine_type = "f1-micro"
- zone         = "europe-west6-a"
+  name         = "hopr-develop-eu-core-001-west6-a"
+  machine_type = "f1-micro"
+  zone         = "europe-west6-a"
 
- boot_disk {
-   initialize_params {
-     image = "debian-cloud/debian-9"
-   }
- }
- network_interface {
-   network = "default"
-   access_config {
-     // Include this section to give the VM an external ip address
-   }
- }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
 }
