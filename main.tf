b1abe6306a25e7ce1ec2c1ffdf43d44de8a5dc7a
Jose Aguinaga <me@jjperezaguinaga.com>
2020-05-15 13:54:12 +0200

    🎉 Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+ credentials = file("credentials.json")
+ project     = "hoprassociation"
+ region      = "europe-west6"
+}
+
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ name         = "hopr-develop-eu-core-001-west6-a"
+ machine_type = "f1-micro"
+ zone         = "europe-west6-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+ network_interface {
+   network = "default"
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
