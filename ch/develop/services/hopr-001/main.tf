e9cd01f505957f2b0fb086d711ad20bfde19980e
Jose Aguinaga <me@jjperezaguinaga.com>
2020-05-26 17:59:11 +0200

    🐛 Making it a bootstrap server

--- ch/develop/services/hopr-001/main.tf
+++ ch/develop/services/hopr-001/main.tf
@@ -23,7 +23,7 @@ module "hopr-node" {
   container_image       = "gcr.io/hoprassociation/hopr-core:latest"
   env_ETHEREUM_PROVIDER = "wss://kovan.infura.io/ws/v3/f7240372c1b442a6885ce9bb825ebc36"
   env_HOST_IPV4         = "0.0.0.0:9091"
-  env_BOOTSTRAP_SERVERS = "/dns4/bootstrap01.hoprnet.io/tcp/9091/p2p/16Uiu2HAm3mkxuhtz2q8aFdNtUXkpp7T3Pqk6pZThtYEnVYetPhyo"
+  env_BOOTSTRAP_SERVERS = ""
   container_arguments   = ["-b", "-p", "123456"]
   key                   = "key.pub"
 }
