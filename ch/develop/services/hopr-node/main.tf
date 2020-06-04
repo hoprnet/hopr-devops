cbc43a69f8f7e80a3c480010d640b049d14a0271
Jose Aguinaga <me@jjperezaguinaga.com>
2020-06-04 20:35:02 +0200

    ✨ Adding random shuffle

--- ch/develop/services/hopr-node/main.tf
+++ ch/develop/services/hopr-node/main.tf
@@ -11,18 +11,26 @@ provider google {
   version = "~> 3.0"
 }
 
-locals {
-  bootstrap_servers = "/ip4/34.65.92.146/tcp/9091/p2p/16Uiu2HAm5HEoRMUSD5fjofEEDRyxhY4hCAwmYgphdF1TPUnzHgNz,/ip4/34.65.237.196/tcp/9091/p2p/16Uiu2HAmHFq9BJS1sxPxo9Bn5A6TEedpCuf6izpX2rA5QDg6qFZa"
+resource "random_shuffle" "bootstrap" {
+  input = [
+    "/dns4/ch-test-01.hoprnet.io/tcp/9091/p2p/16Uiu2HAm2n7Lfn76Ex7JnesxXjCtoPbLA6S1Q3boEELETBCD9RKc",
+    "/dns4/ch-test-02.hoprnet.io/tcp/9091/p2p/16Uiu2HAm9YbgfQ5yAPQ99ZgfCdJKcdn7tZ2xDBwvaaitfvXGgD2d",
+    "/dns4/ch-test-03.hoprnet.io/tcp/9091/p2p/16Uiu2HAmSDWT1xhYc7CYEunDoVY21aWA5LpXTTqZpxymFB34FFjH",
+    "/ip4/34.65.200.251/tcp/9091/p2p/16Uiu2HAm2n7Lfn76Ex7JnesxXjCtoPbLA6S1Q3boEELETBCD9RKc",
+    "/ip4/34.65.51.50/tcp/9091/p2p/16Uiu2HAm9YbgfQ5yAPQ99ZgfCdJKcdn7tZ2xDBwvaaitfvXGgD2d",
+    "/ip4/34.65.69.119/tcp/9091/p2p/16Uiu2HAmSDWT1xhYc7CYEunDoVY21aWA5LpXTTqZpxymFB34FFjH"
+  ]
+  result_count = 1
 }
 
 module "hopr-node" {
-  instance_count = 5
+  instance_count = 6
   instance_name  = "hopr-node"
   prefix         = "ch-develop"
 
   env_ETHEREUM_PROVIDER = "wss://kovan.infura.io/ws/v3/f7240372c1b442a6885ce9bb825ebc36"
   env_HOST_IPV4         = "0.0.0.0:9091"
-  env_BOOTSTRAP_SERVERS = local.bootstrap_servers
+  env_BOOTSTRAP_SERVERS = "${random_shuffle.bootstrap.result}"
   env_DEBUG             = "hopr-core*"
 
   container_arguments = ["-p", "111111"]
