636f2684bdc092540c84b7b197b10cb048624b43
Jose Aguinaga <me@jjperezaguinaga.com>
2020-06-25 16:27:31 +0200

    Updating bootstrap nodes and new version of HOPR Chat

--- ch/develop/services/hopr-node/main.tf
+++ ch/develop/services/hopr-node/main.tf
@@ -3,9 +3,9 @@ locals {
   environment = "develop"
   prefix      = "${local.datacenter}-${local.environment}"
   bootstrap_servers = [
-    "/ip4/34.65.75.45/tcp/9091/p2p/16Uiu2HAmN9QiHLqfCNcG7weNS6TfaLHJA5qB5su7dMitMKAhPLad",
-    "/ip4/34.65.120.13/tcp/9091/p2p/16Uiu2HAm7tHZthPoRuf9Ut84vquCCLmzE4j5LQR9akt6TwMo4GsW",
-    "/ip4/34.65.69.76/tcp/9091/p2p/16Uiu2HAkv5CjUUbhLDRwkAK2krL8VUPC4qhG6RMCdYNvyx4c7n7t"
+    "/ip4/34.65.51.50/tcp/9091/p2p/16Uiu2HAmSSJAVoED4kU2hhgLNcNSTtyG53TWF41Q1ogpYLXFWYiR",
+    "/ip4/34.65.69.119/tcp/9091/p2p/16Uiu2HAmF4SaWWJEAfnSNrcD4ZMX69WvCivqiW6nhHtsRwj2XVaZ",
+    "/ip4/34.65.114.152/tcp/9091/p2p/16Uiu2HAmNJzwT8XUUUSnNCdXFVNxiPnGbffDwpaGGA75ETqsun8R"
   ]
 }
 
@@ -41,7 +41,7 @@ module "hopr-node" {
   region          = "europe-west6"
   zone            = "europe-west6-a"
   client_email    = "terraform@hopr-${local.prefix}.iam.gserviceaccount.com"
-  container_image = "gcr.io/hoprassociation/hopr-core:testnet-52d6767"
+  container_image = "gcr.io/hoprassociation/hopr-core:1.1.3-dev.510badd"
   key             = "key.pub"
 }
 
