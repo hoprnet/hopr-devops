d4f4cb0f75c084f2ef8848603366af63abc40c20
Jose Aguinaga <me@jjperezaguinaga.com>
2021-02-11 15:41:20 +0100

    Re-activating nodes

--- ch/develop/services/hopr-node/main.tf
+++ ch/develop/services/hopr-node/main.tf
@@ -2,10 +2,6 @@ locals {
   datacenter  = "ch"
   environment = "develop"
   prefix      = "${local.datacenter}-${local.environment}"
-  bootstrap_servers = [
-    "/ip4/34.65.237.196/tcp/9091/p2p/16Uiu2HAmFTN6aGQDPcdFRrLWTJc989vunB9mXKwMvBYykRNgKzrD",
-    "/ip4/34.65.51.50/tcp/9091/p2p/16Uiu2HAmEDhVpYCbjhJD9K1kYupyjFeZ4ZjikeyPx2i2Y29gHgKb"
-  ]
 }
 
 terraform {
@@ -22,14 +18,11 @@ provider google {
 }
 
 module "hopr-node" {
-  instance_count    = 0
+  instance_count    = 1
   instance_name     = "hopr-node"
   prefix            = local.prefix
-  bootstrap_servers = local.bootstrap_servers
   is_bootstrap      = false
 
-  env_ETHEREUM_PROVIDER = "wss://kovan.infura.io/ws/v3/f7240372c1b442a6885ce9bb825ebc36"
-  env_HOST_IPV4         = "0.0.0.0:9091"
   env_DEBUG             = "hopr-core*"
 
   container_arguments = ["-p", "111111"]
@@ -40,7 +33,7 @@ module "hopr-node" {
   region          = "europe-west6"
   zone            = "europe-west6-a"
   client_email    = "terraform@hopr-${local.prefix}.iam.gserviceaccount.com"
-  container_image = "gcr.io/hoprassociation/hopr-chat:v1.3.1"
+  container_image = "gcr.io/hoprassociation/hoprd:1.69.3"
   key             = "key.pub"
 }
 
